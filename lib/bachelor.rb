f671efe90776f0d076604e16fe276bc4c241d058
Matthew Sherman <msherman0801@gmail.com>
2018-11-03 21:43:52 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,61 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+
+def get_first_name_of_season_winner(hash, season)
+  hash.each do |k,v|
+    if k == season
+      v.each do |key|
+      if key["status"] == "Winner"
+          return key["name"].split(' ')[0]
+      end
+      end
+    end
+  end
 end
 
-def get_contestant_name(data, occupation)
-  # code here
+def get_contestant_name(hash, occupation)
+  hash.each do |k,v|
+      v.each do |person|
+        if person["occupation"] == occupation
+          return person["name"]
+        end
+      end
+    end
+  end
+
+
+def count_contestants_by_hometown(hash, hometown)
+  counter = 0
+    hash.each do |k,v|
+      v.each do |person|
+        if person["hometown"] == hometown
+          counter += 1
+        end
+      end
+    end
+  counter
 end
 
-def count_contestants_by_hometown(data, hometown)
-  # code here
+def get_occupation(hash, hometown)
+    hash.each do |k,v|
+      v.each do |person|
+        if person["hometown"] == hometown
+          return person["occupation"]
+        end
+      end
+    end
 end
 
-def get_occupation(data, hometown)
-  # code here
-end
-
-def get_average_age_for_season(data, season)
-  # code here
+def get_average_age_for_season(hash, season)
+  out = 0
+  count = 0
+  hash.each do |k,v|
+    if k == season
+      v.each do |person|
+        count += 1 
+        out += person["age"].to_f
+      end
+    end
+  end
+  out = out/count
+  out.round
 end
